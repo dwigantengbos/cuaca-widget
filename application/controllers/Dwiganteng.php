040b353f8f0260a93a9fa12f23cb5a198d294aef
=Dwi Cahyo <=decepeea@gmail.com>
2020-03-01 14:24:57 +0700

    menambahkan coding cuace ape ke tadek ye

--- application/controllers/Dwiganteng.php
+++ application/controllers/Dwiganteng.php
@@ -1,15 +1,32 @@
-<?php
+<?php if
+( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 
 
 class Dwiganteng extends CI_Controller{
     
-
     
     public function index()
-    {
-        
-    $data = []; //array
-    $data['name'] = "DWI GANTENG BOS";
-    $this->load->view('Dwiganteng', $data);
-    }
+	 {
+	 	// $this->load->helper('custom');
+	 	// tampilkan_error('test');
+	 	
+	 	$api = new RestClient([			
+		    'base_url' => 'https://ibnux.github.io/BMKG-importer',	
+		    'format' => "json",	    
+		    'headers' => [
+		    	
+		    ],
+		    'curl_options' => [
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false
+            ]
+		]);
+
+	 	$result = $api->get("cuaca/501320");
+	 	$data = []; //array();
+	 	$data['name'] = "DWI GANTENG BOS";
+	 	$data['data'] = $result->decode_response(); 
+	 	$this->load->view('Dwiganteng', $data);
+	 }
 }
--- application/views/Dwiganteng.php
+++ application/views/Dwiganteng.php
@@ -6,6 +6,28 @@
 <body>
 
 <p>NAMA SAYA <?php echo $name ?></p>
+    <h3>Ngujum Cuace</h3>
 
+    <ul> 
+        
+        
+   <?php foreach((array) $data as $item) { ?>
+
+
+   <?php $date = Carbon\Carbon::createFromFormat('Y-m-d H:i:s',
+          @$item->jamCuaca);  ?>
+        
+        <li> Cuace kinnitok kota singkawang hari <?php echo  @$date->format('l');?> 
+            tanggal <?php echo  @$date->format('d M Y'); ?>
+            jam <?php echo  @$date->format('H:i'); ?>
+            adalah <?php echo $item->cuaca; ?>
+            dengan suhu <?php echo $item->tempC; ?>
+            derajat celcius.
+        
+   
+        </li>
+ <?php }?>
+    
+    </ul>
 </body>
 </html>
